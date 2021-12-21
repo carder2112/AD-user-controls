41d7f4dad2193d8887107db3a3b4f34de9f4b9ce
mcarder <mcardercarboline@gmail.com>
2021-12-21 14:10:00 -0600

    Added script and test CSV

---
+++ Update AD user info/Update AD user info.ps1
@@ -0,0 +1,32 @@
+#This script will update AD user info from a CSV file with these fields populated:
+
+
+
+Import-Module ActiveDirectory
+
+$userinfo = Import-Csv "csv path"
+
+foreach($user in $userinfo){
+
+    # Find user
+	
+    $ADUser = Get-ADUser -Filter "EmailAddress -eq '$user.PrimaryEmail'" -Properties mail
+	
+	
+    if ($ADUser){
+        Set-ADUser -Identity $ADUser 
+			-Company $user.BusinessUnit 
+			-Department $user.Department 
+			-Title $user.jobtitle 
+			-Office $user.Office 
+			-Manager $user.Manager 
+			-OfficePhone $user.OfficePhone
+			-StreetAddress $user.StreetAddress
+			-City $user.City
+			-State $user.State
+			-PostalCode $user.Zip
+			-Country $user.Country
+    }else{
+        Write-Warning ("Failed to update " + $($user.name))
+    }
+}
