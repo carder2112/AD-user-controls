aa491ee719968844e886b6e879e2697885ba0a01
mcarder <mcardercarboline@gmail.com>
2021-12-22 14:28:09 -0600

    Update script to check for empty CSV fields
    Added if statements to make sure there was valid input. A blank in the CSV would make the entire row invalid before this update.

--- Update AD user info/Update AD user info.ps1
+++ Update AD user info/Update AD user info.ps1
@@ -13,8 +13,42 @@ foreach($user in $userinfo){
     $ADUser = Get-ADUser -Filter "EmailAddress -eq '$($user.PrimaryEmail)'" -Properties mail
 	
 	
+	#The following if statements check to see if the user exists then ignores empty fields in the CSV
+	
     if ($ADUser){
-        Set-ADUser -Identity $ADUser -Company $user.BusinessUnit -Department $user.Department -Title $user.jobtitle -Office $user.Office -Manager $user.Manager -OfficePhone $user.OfficePhone -StreetAddress $user.StreetAddress -City $user.City -State $user.State -PostalCode $user.Zip -Country $user.Country
+		if ($user.BusinessUnit) {
+			Set-ADUser -Identity $ADUser -Company $user.BusinessUnit
+		}
+		if ($user.Department) {
+			Set-ADUser -Identity $ADUser -Department $user.Department
+		}
+		if ($user.jobtitle) {
+			Set-ADUser -Identity $ADUser -Title $user.jobtitle
+		}
+		if ($user.Office) {
+			Set-ADUser -Identity $ADUser -Office $user.Office
+		}
+		if ($user.Manager) {
+			Set-ADUser -Identity $ADUser -Manager $user.Manager
+		}
+		if ($user.OfficePhone) {
+			Set-ADUser -Identity $ADUser -OfficePhone $user.OfficePhone
+		}
+		if ($user.StreetAddress) {
+			Set-ADUser -Identity $ADUser -StreetAddress $user.StreetAddress
+		}
+		if ($user.City) {
+			Set-ADUser -Identity $ADUser -City $user.City
+		}
+		if ($user.State) {
+			Set-ADUser -Identity $ADUser -State $user.State
+		}
+		if ($user.Zip) {
+			Set-ADUser -Identity $ADUser -PostalCode $user.Zip
+		}
+		if ($user.Country) {
+			Set-ADUser -Identity $ADUser -Country $user.Country
+		}
     }else{
         Write-Warning ("Failed to update " + $user.PrimaryEmail)
     }
